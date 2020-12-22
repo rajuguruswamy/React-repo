2352d755e7add399ae20da13adb5bd292ab19dc1
rajuguruswamy <raju.guruswamy.sg@gmail.com>
2020-12-22 21:53:07 +0800

    initial commit for react app

---
+++ frontend/src/data.js
@@ -0,0 +1,73 @@
+const data = {
+
+   products :[
+       {
+           _id:'1',
+           name:'Nike Slim Shirt',
+           category:'Shirts',
+           image:'/images/p1.jpg',
+           price:120,
+           brand:'Nike',
+           ranking:4.5,
+           numReviews:10,
+           description: 'Hi quality product'
+       },
+       {
+        _id:'2',
+        name:'Adidas Fit Shirt',
+        category:'Shirts',
+        image:'/images/p2.jpg',
+        price:100,
+        brand:'Adidas',
+        ranking:4,
+        numReviews:10,
+        description: 'Hi quality product'
+    },
+    {
+        _id:'3',
+        name:'Lacoste free Shirt',
+        category:'Shirts',
+        image:'/images/p3.jpg',
+        price:220,
+        brand:'Lacoste',
+        ranking:4,
+        numReviews:17,
+        description: 'Hi quality product'
+    },
+    {
+        _id:'4',
+        name:'Nike Slim Pants',
+        category:'Pants',
+        image:'/images/p4.jpg',
+        price:78,
+        brand:'Nike',
+        ranking:4.5,
+        numReviews:14,
+        description: 'Hi quality product'
+    },
+    {
+        _id:'5',
+        name:'c Slim Shirt',
+        category:'Pants',
+        image:'/images/p5.jpg',
+        price:65,
+        brand:'Puma',
+        ranking:4.5,
+        numReviews:10,
+        description: 'Hi quality product'
+    },
+    {
+        _id:'6',
+        name:'Adidas Fit Pants',
+        category:'Pants',
+        image:'/images/p6.jpg',
+        price:90,
+        brand:'Adidas',
+        ranking:4,
+        numReviews:12,
+        description: 'Hi quality product'
+    }
+   ] 
+}
+
+export default data;
